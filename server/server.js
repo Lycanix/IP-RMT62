46a7b6fc9c23817e288c215b42a4ea003b0d4644
Rama <syahrul.rama33@gmail.com>
2025-06-25 12:37:11 +0700

    refactor: edit google auth

--- server/server.js
+++ server/server.js
@@ -2,5 +2,6 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+	console.log("JWT_SECRET loaded:", process.env.JWT_SECRET);
 	console.log(`Server running at http://localhost:${PORT}`);
 });
